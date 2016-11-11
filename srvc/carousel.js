baba70fad6d16641a94b68383de6f4396550e21b
Omkar Kulkarni <mail.omkar.kulkarni@gmail.com>
2016-11-11 13:05:31 +0530

    Update carousel.js

--- srvc/carousel.js
+++ srvc/carousel.js
@@ -6,25 +6,25 @@ angular.module('CarouselExperiment').service('$carousel',
       "id": "abc",
     "title":"Hello World",
     "subtext":"These words revolutionalised Tech Industry.",
-    "background":"public/img/carousel-static/4.jpg"
+    "background":"https://geekosk.github.io/mdCarousel/public/img/carousel-static/4.jpg"
   },
   {
       "id": "abc1",
     "title":"1:45 AM",
     "subtext":"Its the time when I created this.",
-    "background":"public/img/carousel-static/2.jpg"
+    "background":"https://geekosk.github.io/mdCarousel/public/img/carousel-static/2.jpg"
   },
   {
       "id": "abc2",
     "title":"NG Carousel",
     "subtext":"This is made by AngularJS",
-    "background":"public/img/carousel-static/1.jpg"
+    "background":"https://geekosk.github.io/mdCarousel/public/img/carousel-static/1.jpg"
   },
   {
       "id": "abc3",
     "title":"Thats it!",
     "subtext":"I just don't know What to say next...",
-    "background":"public/img/carousel-static/3.png"
+    "background":"https://geekosk.github.io/mdCarousel/public/img/carousel-static/3.png"
   }
 ]
 
