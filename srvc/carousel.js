656debec20d78c947d554ef8ee3b7fb4fcb4b469
Omkar Kulkarni <mail.omkar.kulkarni@gmail.com>
2016-11-11 10:45:24 +0530

    Update carousel.js

--- srvc/carousel.js
+++ srvc/carousel.js
@@ -1,28 +1,31 @@
 angular.module('CarouselExperiment').service('$carousel',
     [
     function(){
-        this.data = [{
-            "id": "abc",
-            "title":"Hello World",
-            "subtext":"Well we make the world better by fast prototyping facilities.",
-            "background":"public/img/carousel-static/4.jpg"
-        },
-            {
-                "id": "abc1",
-                "title":"1:45",
-                "subtext":"A range of products that would keep you awake and productive during day as well as night.",
-                "background":"public/img/carousel-static/2.jpg"
-            },
-            {
-                "id": "abc2",
-                "title":"Firebase Realtime Database",
-                "subtext":"A Database which you just don't need to take care of!",
-                "background":"public/img/carousel-static/1.jpg"
-            },
-            {
-                "id": "abc3",
-                "title":"Microsoft Visual Studio",
-                "subtext":"A debugger which loves your code...",
-                "background":"public/img/carousel-static/3.png"
-            }];
+        this.data = [
+  {
+      "id": "abc",
+    "title":"Hello World",
+    "subtext":"These words revolutionalised Tech Industry.",
+    "background":"public/img/carousel-static/4.jpg"
+  },
+  {
+      "id": "abc1",
+    "title":"1:45 AM",
+    "subtext":"Its the time when I created this.",
+    "background":"public/img/carousel-static/2.jpg"
+  },
+  {
+      "id": "abc2",
+    "title":"NG Carousel",
+    "subtext":"This is made by AngularJS",
+    "background":"public/img/carousel-static/1.jpg"
+  },
+  {
+      "id": "abc3",
+    "title":"Thats it!",
+    "subtext":"I just don't know What to say next...",
+    "background":"public/img/carousel-static/3.png"
+  }
+]
+
 }]);
